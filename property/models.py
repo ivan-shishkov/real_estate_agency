1335eceabffdae12f5f2b910084253e6d45450c8
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-08-14 21:13:55 +0300

    Added Compliant model

--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -26,3 +27,16 @@ class Flat(models.Model):
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
+
+
+class Complaint(models.Model):
+    class Meta:
+        verbose_name = 'жалоба'
+        verbose_name_plural = 'жалобы'
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='кто жаловался')
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name='квартира, на которую пожаловались')
+    text = models.TextField('текст жалобы')
+
+    def __str__(self):
+        return self.text[:30]
