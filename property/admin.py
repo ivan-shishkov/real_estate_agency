d8e880a2db2967792fcb87a9f3fa4aaf7a944729
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-08-15 21:04:56 +0300

    Added display of owners phone numbers for Flat model in admin interface

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,15 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner',)
     readonly_fields = ('created_at',)
 
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
+    list_display = (
+        'address',
+        'price',
+        'new_building',
+        'construction_year',
+        'town',
+        'owners_phonenumber',
+        'owners_phone_pure',
+    )
     list_editable = ('new_building',)
     list_filter = ('new_building',)
 
