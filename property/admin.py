54b90196193a5b50fc9510a0cbd769c12634f538
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-08-16 16:19:56 +0300

    Added display of flat owner model in admin interface

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat, Complaint
+from .models import Flat, Complaint, Owner
 
 
 class FlatAdmin(admin.ModelAdmin):
@@ -26,6 +26,12 @@ class CompliantAdmin(admin.ModelAdmin):
     raw_id_fields = ('author', 'flat')
 
 
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flats',)
+
+
 admin.site.register(Flat, FlatAdmin)
 
 admin.site.register(Complaint, CompliantAdmin)
+
+admin.site.register(Owner, OwnerAdmin)
