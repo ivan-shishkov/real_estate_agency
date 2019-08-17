90fd65f89fd227ac90843f9ead31d0b35cb6d91b
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-08-17 14:44:47 +0300

    Enhanced display of models in admin interface

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address',)
     readonly_fields = ('created_at',)
@@ -10,20 +11,31 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
     list_editable = ('new_building',)
     list_filter = ('new_building',)
+    list_per_page = 50
 
     raw_id_fields = ('liked_by',)
 
+    fieldsets = (
+        (None, {
+            'fields': ('created_at', 'active', 'description', 'price', 'liked_by',),
+        }),
+        ('Расположение', {
+            'fields': ('town', 'town_district', 'address', 'floor',),
+        }),
+        ('Информация о доме', {
+            'fields': ('construction_year', 'new_building',),
+        }),
+        ('Характеристики квартиры', {
+            'fields': ('rooms_number', 'living_area', 'has_balcony'),
+        }),
+    )
 
+
+@admin.register(Complaint)
 class CompliantAdmin(admin.ModelAdmin):
     raw_id_fields = ('author', 'flat',)
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
-
-
-admin.site.register(Flat, FlatAdmin)
-
-admin.site.register(Complaint, CompliantAdmin)
-
-admin.site.register(Owner, OwnerAdmin)
